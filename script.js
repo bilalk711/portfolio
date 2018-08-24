368fa52d27c7216a6611759ad8737ddda8f74f78
bilu7111 <39033434+bilu7111@users.noreply.github.com>
2018-08-24 23:42:29 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+  var paragraph=document.getElementById("Para");
+  var pageHeader=document.getElementById("pageHeader");
+
+  function showPara(self){
+
+    self.style.display="none";
+    paragraph.style.display="block";
+    pageHeader.style.display="block";
+    
+  }
